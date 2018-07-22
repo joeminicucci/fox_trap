dcea7d17e57f84968faa4f17cbf0ced37f81c360
joeminicucci <joeisadev@sudomail.com>
2018-07-22 01:53:00 -0600

    Channel hopping and cleanup

--- fox_bot/src/main.cpp
+++ fox_bot/src/main.cpp
@@ -30,32 +30,35 @@ extern "C"
 #define   MESH_PORT       5566
 
 //Scheduling Intervals
-#define CHANNEL_HOP_INTERVAL_MS   100
+#define RX_PER_CHANNEL_HOP 3
 #define RX_COMM_INTERVAL 10000
 
 //mesh members...NO_OBJECTS == NO_HOPE
 bool botInitialization();
 bool initializeSniffer();
 void channelHop();
-void killEverything();
-void killEverything2();
+void meshDisabled();
+void snifferDisabled();
+static void showMetaData();
+uint8_t MostSignificantByte(uint32_t bytes);
 void receivedCallback( uint32_t from, String &msg );
 uint32_t CalculateSynchronizationDelay();
 
 
 // Prototype
 uint16_t _channel = 6;
+// uint16_t _channelCount = 1;
 size_t logServerId = 0;
 int32_t _startTime = 0;
-uint32_t _synchInterval = 10000; //ms
+uint32_t _synchInterval = 15000; //ms
 unsigned long _initDelay = 15000000;
 
 // void receivedCallback( uint31_t from, String &msg );
 Scheduler     _userScheduler; // to control your personal task
 painlessMesh  _mesh;
-Task _botInitializationTask(_synchInterval, TASK_ONCE, &botInitialization, &_userScheduler, true, NULL, NULL);
-Task channelHopTask(CHANNEL_HOP_INTERVAL_MS, TASK_FOREVER, &channelHop);
-Task _snifferInitializationTask(_synchInterval, TASK_ONCE, &initializeSniffer, &_userScheduler, true, NULL, &killEverything2);
+Task _botInitializationTask(_synchInterval, TASK_ONCE, &botInitialization, &_userScheduler, true, NULL, &meshDisabled);
+//Task _channelHopTask(TASK_IMMEDIATE, TASK_FOREVER, &channelHop, &_userScheduler, true, NULL, NULL);
+Task _snifferInitializationTask(_synchInterval, TASK_ONCE, &initializeSniffer, &_userScheduler, true, NULL, &snifferDisabled);
 uint32_t roundUp(uint32_t numToRound, uint32_t multiple);
 
 
@@ -116,50 +119,48 @@ static void showMetadata(SnifferPacket *snifferPacket) {
   if (frameType != TYPE_MANAGEMENT ||
       frameSubType != SUBTYPE_PROBE_REQUEST)
         return;
-  Serial.print(" Sensor #: ");
-  Serial.print(SENSOR_ID);
 
   Serial.print("RSSI: ");
   Serial.print(snifferPacket->rx_ctrl.rssi, DEC);
 
   Serial.print(" Ch: ");
-  Serial.print(wifi_get_channel());
+Serial.print(wifi_get_channel());
 
-  char addr[] = "00:00:00:00:00:00";
-  getMAC(addr, snifferPacket->data, 10);
-  Serial.print(" BSSID: ");
-  Serial.print(addr);
+char addr[] = "00:00:00:00:00:00";
+ getMAC(addr, snifferPacket->data, 10);
+ Serial.print(" MAC: ");
+Serial.print(addr);
 
-  //uint8_t SSID_length = snifferPacket->data[25];
-  //Serial.print(" SSID: ");
-  //printDataSpan(26, SSID_length, snifferPacket->data);
-
+  // uint8_t SSID_length = snifferPacket->data[24];
+  // Serial.print(" SSID: ");
+  // printDataSpan(25, SSID_length, snifferPacket->data);
+  // if (_channelCount % 2 == -1){
+  //     _channelCount = 0;
+      channelHop();
+    // }
   Serial.println();
 }
 
-/**
- * Callback for promiscuous mode
- */
-static void ICACHE_FLASH_ATTR sniffer_callback(uint8_t *buffer, uint16_t length) {
-  struct SnifferPacket *snifferPacket = (struct SnifferPacket*) buffer;
-  showMetadata(snifferPacket);
-}
+//promiscuous mode
+ static void ICACHE_FLASH_ATTR sniffer_callback(uint8_t *buffer, uint16_t length) {
+   struct SnifferPacket *snifferPacket = (struct SnifferPacket*) buffer;
+   showMetadata(snifferPacket);
+ }
 
 static void printDataSpan(uint16_t start, uint16_t size, uint8_t* data) {
   for(uint16_t i = start; i < DATA_LENGTH && i < start+size; i++) {
     Serial.write(data[i]);
   }
 }
-/**
- * Callback for channel hoping
- */
+
 void channelHop()
 {
   // hoping channels 1-14
   uint8 new_channel = wifi_get_channel() + 1;
-  if (new_channel > 14)
+  if (new_channel > 13)
     new_channel = 1;
   wifi_set_channel(new_channel);
+  // _channelCount+=1;
 }
 //*************** DEMARC SNIFFER ***************/
 
@@ -186,62 +187,59 @@ Task myLoggingTask(RX_COMM_INTERVAL, TASK_FOREVER, []() {
 bool botInitialization()
 {
     _snifferInitializationTask.disable();
+    // _channelHopTask.disable();
     wifi_promiscuous_enable(DISABLE);
 
       _snifferInitializationTask.restartDelayed(_synchInterval);
-    //_mesh.stop();
-    //_snifferInitializationTask.disable();
-    Serial.printf("BOT:botInitialization");
+      // _channelHopTask.restartDelayed(_synchInterval);
+    Serial.printf("BOT:botInitialization\n");
 
     _mesh.setDebugMsgTypes(ERROR | CONNECTION);  // set before init() so that you can see startup messages
     _mesh.init( MESH_PREFIX, MESH_PASSWORD, &_userScheduler, MESH_PORT, WIFI_AP_STA, _channel);
 
     //Wait for a specified amount of time to gather the longest RTT from c2..not a brillaint design decision
     //_botInitializationTask.delay(_initDelay);
-    //instead of using an async delay, delay all execution so that the sniffer continues its job
-    Serial.printf("BOT:botInitialization_END");
+    Serial.printf("BOT:botInitialization_END\n");
     return true;
 }
+
+bool initializeSniffer(){
+    _botInitializationTask.disable();
+    // _botInitializationTask.delay(_synchInterval);
+    _botInitializationTask.restartDelayed(_synchInterval);
+    wifi_set_opmode(STATION_MODE);
+    wifi_promiscuous_enable(ENABLE);
+    Serial.printf("promiscuos enable\n");
+    return true;
+}
+
 
 void setup() {
   Serial.begin(115200);
   // _botInitializationTask new Task(TASK_IMMEDIATE, TASK_ONCE, &botInitialization, &_userScheduler);
-  // Serial.printf("logClient ID:%u",_mesh.getNodeId());
   _userScheduler.addTask(_botInitializationTask);
   _userScheduler.addTask(_snifferInitializationTask);
-
-  //_userScheduler.addTask(_snifferInitializationTask)
-    //Calculate synch delay, get offset of delay to current time and either use set or restart
-
+  // _userScheduler.addTask(_channelHopTask);
+  //Calculate synch delay, get offset of delay to current time and either use set or restart
     Serial.printf("TIME DELAY is: %i mS\n", _synchInterval);
-    int synchDelay = CalculateSynchronizationDelay();
+
+    wifi_set_promiscuous_rx_cb(sniffer_callback);
+    Serial.printf("promiscuos callback set\n");
     //keep the topology from fucking itself
     _mesh.setRoot(false);
     // This and all other mesh should ideally now the mesh contains a root
     _mesh.setContainsRoot(true);
     _mesh.onReceive(&receivedCallback);
+    // _mesh.init( MESH_PREFIX, MESH_PASSWORD, &_userScheduler, MESH_PORT, WIFI_AP_STA, _channel);
 
-    // delay(synchDelay);
-    // _botInitializationTask.setInterval(_synchInterval);
+    uint32_t synchDelay = CalculateSynchronizationDelay();
+    Serial.printf("next sync is :%i \n",synchDelay);
+    Serial.printf("msb of syncDelay is:%i \n",MostSignificantByte(synchDelay));
+
     _botInitializationTask.enable();
-  //_botInitializationTask.restartDelayed();
-//delay(_synchInterval);
-// _botInitializationTask.disable();
-
-  // _botInitializationTask.setInterval(_synchInterval);
-  //delay(_synchInterval / 2 );
-
   //_snifferInitializationTask.enable();
-  Serial.printf("BOT:SETUP with start time of %i", _startTime);
 
-  // Add the task to the your scheduler
-  // _userScheduler.addTask(myLoggingTask);
-  // _userScheduler.addTask(channelHopTask);
-  // myLoggingTask.enable();
-  // channelHopTask.enable();
-
-  //SNIFFER
-  //delay(10);
+  Serial.printf("BOT:SETUP with start time of %i\n", _startTime);
 }
 
 void loop() {
@@ -249,20 +247,14 @@ void loop() {
     _mesh.update();
 }
 
-void killEverything(){
-    //_userScheduler.disableAll( true);
-    //_mesh.stop();
-    //_snifferInitializationTask.enable();
-    Serial.printf("GOODBYE BOT. Hello Sniffy");
+void meshDisabled(){
+
+    Serial.printf("GOODBYE BOT. Hello Sniffy\n");
 }
-void killEverything2(){
-    //_userScheduler.disableAll( true);
-    //_mesh.stop();
-    //_botInitializationTask.enable();
-    //wifi_promiscuous_enable(DISABLE);
+void snifferDisabled(){
     //STA_AP mode
-    wifi_set_opmode(0x03);
-    Serial.printf("GOODBYE SNIFFY. Hello bot");
+    wifi_set_opmode(STATIONAP_MODE);
+    Serial.printf("GOODBYE SNIFFY. Hello bot\n");
 }
 
 
@@ -278,25 +270,6 @@ void receivedCallback( uint32_t from, String &msg ) {
             : _startTime;
     }
       // Serial.printf("logCleint: Handled from %u msg=%s\n", from, msg.c_str());
-}
-
-bool initializeSniffer(){
-    _botInitializationTask.disable();
-    //_mesh.stop();
-
-    // _botInitializationTask.delay(_synchInterval);
-    _botInitializationTask.restartDelayed(_synchInterval);
-    wifi_set_opmode(STATION_MODE);
-    //_mesh.init( MESH_PREFIX, MESH_PASSWORD, &_userScheduler, MESH_PORT, WIFI_STA, _channel);
-
-    wifi_set_channel(1);
-    // wifi_promiscuous_enable(DISABLE);
-    // Serial.printf("promiscuos disable\n");
-    wifi_set_promiscuous_rx_cb(sniffer_callback);
-    Serial.printf("promiscuos callback set\n");
-    wifi_promiscuous_enable(ENABLE);
-    Serial.printf("promiscuos enable\n");
-    return true;
 }
 
 uint32_t CalculateSynchronizationDelay(){
@@ -324,17 +297,17 @@ uint32_t roundUp(uint32_t numToRound, uint32_t multiple)
     return numToRound + multiple - remainder;
 }
 
-uint8_t MostSignificantBit()
+uint8_t MostSignificantByte(uint32_t blockSize)
 {
   uint8_t bytesLen[sizeof(uint32_t)];
-  uint32_t blockSize = 535;
+  // uint32_t blockSize = 535;
 
  bytesLen[3] = (blockSize >>  0) & 0xFF;
  bytesLen[2] = (blockSize >>  8) & 0xFF;
  bytesLen[1] = (blockSize >> 16) & 0xFF;
  bytesLen[0] = (blockSize >> 24) & 0xFF;
 
- bool removeZeroes = true;
+ // bool removeZeroes = true;
  // std::cout << "bytesLen: 0x";
  for(size_t i=0; i<sizeof(bytesLen); i++)
  {
