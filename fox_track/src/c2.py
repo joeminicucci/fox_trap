5b171ba2cd896d837b5d1d22fbe941cd3b485f78
joeminicucci <joeisadev@sudomail.com>
2018-08-27 21:23:19 -0600

    c2.py refinement

--- fox_track/src/c2.py
+++ fox_track/src/c2.py
@@ -1,80 +1,6 @@
 #!/usr/bin/python
+import serial, subprocess, sys, argparse, re, datetime
 
-import serial, subprocess, sys, argparse, re, aenum, datetime
-from aenum import Enum
-
-
-
-# ser = serial.Serial()
-# ser.baudrate = 115200
-# ser.port = '/dev/ttyUSB0'
-# ser.timeout = 1.5  # IMPORTANT, can be lower or higher
-# #ser.open()
-# if ser.isOpen():
-#     ser.close()
-# ser.open()
-# ser = serial.Serial('/dev/tty0', 115200, timeout=2.5)
-
-# #attempt 1
-# while 1:
-#     ser.flushInput()
-#     ser.write("SHITTY MCSHIT SHIT"'\n')
-#     print ser.readline()
-
-# # attempt 2
-# while True:
-#     cmd = raw_input("> ");
-#     ser.write(cmd + "\r\n")
-#     ret = ser.read(len(cmd)) # eat echo
-#     time.sleep( 0.2 )
-#     while ( ser.inWaiting() ):
-#         ret = ser.readline().rstrip()
-#         print ret
-
-# attempt 3
-# while 1:
-#     #print("attempting serial read")
-#     #serial_line = ser.readline()
-#
-#     bytesToRead = ser.inWaiting()
-#     #print bytesToRead
-#     while bytesToRead:
-#         serial_line = ser.readline(bytesToRead)
-#         print serial_line
-#
-#         if not serial_line.strip():
-#             continue
-# print(serial_line) # If using Python 2.x use: print serial_line
-# Do some other work on the data
-
-# time.sleep(300) # sleep 5 minutes
-
-#     # Loop restarts once the sleep is finished
-
-# ser.close() # Only executes once the loop exits
-
-class Alias(Enum):
-
-    Donald_Duck = 10607203
-    Mickey = 261400
-    Carl = 357003
-    Hawk_Eye = 10607977
-    Radar = 310418
-    Baloo = 6702602
-    TRex = 357031
-    Popeye = 10607966
-    Minnie = 3943002751
-    Belle = 10607198
-    # Ariel =
-    # Stitch
-
-    @classmethod
-    def sub(cls, line, escape):
-        # if escape:
-        aliasedLine = re.sub('\d{10}', lambda v: "\033[11;97m" + cls(int(v.group())).name + "\033[m", line)
-        # else:
-            # aliasedLine = re.sub('\d{10}', lambda v: "\033[11;97m" + cls(int(v.group())).name + "\033", line)
-        return aliasedLine
 
 def run_command(command):
     p = subprocess.Popen(command,
@@ -95,10 +21,10 @@ def handle_c2_line(line, signalUserId, signalGroupId):
     # line_pieces = re.split(r'\s+',line)
     line_pieces = line.split('|')
     if line_pieces and line_pieces[0].startswith("[FOUND]"):
-        print("Begin signal comm")
-        run_signal_comm(signalUserId,signalGroupId,line)
-        print("End signal comm")
-        # newstuff = stuff.replace(" ","")
+        if signalMode:
+            print("Begin signal comm")
+            run_signal_comm(signalUserId,signalGroupId,line)
+            print("End signal comm")
         mac = line_pieces[1].strip()
         channel = line_pieces[2].strip()
 
@@ -108,17 +34,13 @@ def handle_c2_line(line, signalUserId, signalGroupId):
             text_file.write(channel)
 
         print('\x1b[6;30;42m' + str(datetime.datetime.time(datetime.datetime.now())) + '\x1b[0m')
-        # print('\x1b[6;30;42m' + Alias.sub(line,False) + '\x1b[0m')
         print('\x1b[6;30;42m' + line + '\x1b[0m')
-        time.sleep(3)
         sys.exit()
 
     elif line:
         print (line)
-        # print(Alias.sub(line,True))
 
-#
-#
+signalMode=False
 def main(argv):
     parser = argparse.ArgumentParser(description='Keep track of a sniffly mesh network and issue signal messages accordingly')
     parser.add_argument('-s', "--serialPort",
@@ -142,12 +64,14 @@ def main(argv):
         sys.exit()
     signalUserId = args.signalUserId
     signalGroupId = args.signalGroupId
+    signalMode = args.signalUserId is not None
     mode = args.mode
 
     # print ("TESTING SIGNAL OUT: " + 'signal-cli -u %s -m %s -g %s' % (signalUserId, signalGroupId, 'gi'))
     print 'Serial: ', serialPort
     print 'signalUserId: ', signalUserId
     print 'signalGroupId: ', signalGroupId
+    print 'signalMode: ', signalMode
     print 'mode: ', mode
 
     try:
